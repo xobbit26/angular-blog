25381259c4ec2e82f0231fa1794d0fc2308e4e6d
Stanislav Naravetskas <stanislav.naravetskas@sdworx.com>
2022-06-04 20:57:30 +0300

    added home and admin layouts for application

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    EditPageComponent,
+    CreatePageComponent,
+    DashboardPageComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    RouterModule.forChild([{
+      path: '', component: AdminLayoutComponent, children: [
+        { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
+        { path: 'login', component: LoginPageComponent }
+        // { path: 'dashboard', component: DashboardPageComponent },
+        // { path: 'create', component: CreatePageComponent },
+        // { path: 'post/:id/edit', component: EditPageComponent }
+      ]
+    }]),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AdminModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
